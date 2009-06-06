32d2fd85271f7a8da0afbca9e02bef82f21204d6
Glenn Fowler <cebjyre@cpan.org>
2009-06-07 01:05:39 +1000

    Documentation
    Setting up the POD documentation.

--- lib/Versionify/Dispatch.pm
+++ lib/Versionify/Dispatch.pm
@@ -58,97 +58,109 @@ This document describes Versionify::Dispatch version 0.0.1
 =head1 SYNOPSIS
 
     use Versionify::Dispatch;
+    
+    my $dispatcher = Versionify::Dispatch->new(
+        function => {
+            1.0 => \&some_function,
+            1.6 => \$sub_ref,
+        }
+    );
+    
+    $dispatcher->get_function($desired_version)->(@args);
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
+
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
+This module is designed to allow a version friendly approach to dispatching
+function calls. One possible use of this is in an API situation, where the
+behaviour of functionality with a similar purpose may change over time.
+
+When the function of a specific version is requested, the reference to the
+function with the highest version equal to or less than the requested version
+is returned by the C<get_function> method. If no specific version is supplied,
+the default version is used (if set), otherwise the maximum version is used.
 
 
 =head1 INTERFACE 
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+=over
+
+=item new
+
+    $dispatcher = Versionify::Dispatch->new(default_version => $some_version, function => \%function_mapping);
+
+The constructor, whic has two named parameters - the default version, and a
+hashref of the version to function mapping. Both parameters are optional, but
+calling C<get_function> without having initialised the function mapping
+(either in the constructor, or in C<set_function>) will not work well.
+
+=item set_function
+
+    $dispatcher->set_function(\%function_mapping);
+
+Used to set the version -> function mapping inside the dispatcher object. B<This
+will remove any existing mapping.>
+
+=item set_default_version
+
+    $dispatcher->set_default_version($some_version);
+
+Sets the default version to be used when no version is explicitly set in
+the C<get_function> call.
+
+=item get_function
+
+    $return = $dispatcher->get_function($version)->(@args);
+    # or
+    $sub_ref = $dispatcher->get_function($version);
+
+Returns the appropriate function. If the version string is not supplied
+the default version is used. The highest versioned function reference
+that is less than or equal to the version (or highest in general if no
+version was supplied and no default is set) is returned.
+
+The version comparisons are done by the L<Sort::Versions> module.
+
+=back
 
 
 =head1 DIAGNOSTICS
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
 =over
 
-=item C<< Error message here, perhaps with %s placeholders >>
+=item C<< No valid functions stored >>
 
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
+There are no functions of a version less than or equal to the desired
+version (possibly no functions at all).
 
 =back
 
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
 Versionify::Dispatch requires no configuration files or environment variables.
 
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
+=over
 
-None.
+=item L<Moose>
+
+=item L<MooseX::FollowPBP>
+
+=item L<Sort::Versions>
+
+=back
 
 
 =head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
 
 None reported.
 
 
 =head1 BUGS AND LIMITATIONS
 
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
 
 No bugs have been reported.
 
@@ -161,13 +173,14 @@ L<http://rt.cpan.org>.
 
 Glenn Fowler  C<< <cebjyre@cpan.org> >>
 
+Thanks to L<http://www.hiivesystems.com>.
 
 =head1 LICENCE AND COPYRIGHT
 
 Copyright (c) 2009, Glenn Fowler C<< <cebjyre@cpan.org> >>. All rights reserved.
 
 This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
+modify it under the same terms as Perl itself.
 
 
 =head1 DISCLAIMER OF WARRANTY
